936e91a514c6e96443cd6bea23acb8bab8870fa9
ProfessorGundar <RyanDL1@hotmail.com>
2022-07-05 22:26:32 -0500

    -Got a working example of matplotlib working

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import math
 import nltk
+import matplotlib.pyplot as plt
 
 def entropy(labels):
     freqdist = nltk.FreqDist(labels) # Will count symbols in a set
     probs = [freqdist.freq(l) for l in freqdist] # Will calculate the probability of said count
+    plt.bar(freqdist.keys(), probs)
     return -sum(p * math.log(p,2) for p in probs) # Calculates entropy for set
 
 wordSet = []
@@ -12,3 +14,4 @@ for i in range(0,14): wordSet.append("Dogs")
 for i in range(0,7): wordSet.append("Lions")
 
 print(entropy(wordSet))
+plt.show()
