0070ee92a47a2005379915fc536d8c818ac413ce
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-07-11 21:56:02 -0500

    -Added ability to read text from harry potter books from github txt file -Tried a regex tokenizer, but im running into issues for "name(.|!|etc)" cases -Added resource to look into next time to find person entitys dynamically

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import math
 import nltk
 import matplotlib.pyplot as plt
+import requests
 
 
 def entropy(labels):
     freqdist = nltk.FreqDist(labels)  # Will count symbols in a set
     probs = [freqdist.freq(l) for l in freqdist]  # Will calculate the probability of said count
     plt.bar(freqdist.keys(), probs)
+    plt.show()
     return -sum(p * math.log(p, 2) for p in probs)  # Calculates entropy for set
 
 
-def tokenize_harry_potter_book_philosopher_stone_for_character_names():
-    return
+def tokenize_harry_potter_book_philosopher_stone():
+    url = "https://raw.githubusercontent.com/formcept/whiteboard/master/nbviewer/notebooks/data/harrypotter/Book%201" \
+          "%20-%20The%20Philosopher's%20Stone.txt"
+    response = requests.get(url)
+    tokenizer = nltk.tokenize.RegexpTokenizer(get_regex_for_all_characters())
+    return tokenizer.tokenize(response.text)
 
 
-def tokenize_harry_potter_book_chamber_of_secrets_for_character_names():
-    return
+def tokenize_harry_potter_book_chamber_of_secrets():
+    url = "https://raw.githubusercontent.com/formcept/whiteboard/master/nbviewer/notebooks/data/harrypotter/Book%202" \
+          "%20-%20The%20Chamber%20of%20Secrets.txt"
+    response = requests.get(url)
+    tokenizer = nltk.tokenize.RegexpTokenizer(get_regex_for_all_characters())
+    return tokenizer.tokenize(response.text)
 
 
 def get_harry_potter_characters():
@@ -23,7 +33,16 @@ def get_harry_potter_characters():
     return harryPotterCharacterNamesFile.read().splitlines()
 
 
-def main():
-    plt.show()
+def get_regex_for_all_characters():
+    regexForAllCharacterNames = ""
+    for character in get_harry_potter_characters():
+        regexForAllCharacterNames += character + "|"
+    return regexForAllCharacterNames[:-1]
 
-# main()
+
+def main():
+    print(entropy(tokenize_harry_potter_book_philosopher_stone()))
+    print(entropy(tokenize_harry_potter_book_chamber_of_secrets()))
+
+
+main()
--- test_sample.py
+++ test_sample.py
@@ -14,8 +14,23 @@ def test_entropy_with_word_set_of_three_words():
 
 
 def test_list_of_harry_potter_characters():
-
     harryPotterCharacters = main.get_harry_potter_characters()
 
-    assert harryPotterCharacters.__sizeof__() > 0
+    assert harryPotterCharacters.__len__() == 38
     assert harryPotterCharacters.count("Harry") == 1
+
+
+def test_text_of_harry_potter_philosopher_stone():
+    tokenizedCharacters = main.tokenize_harry_potter_book_philosopher_stone()
+
+    assert tokenizedCharacters.__len__() > 0
+    assert tokenizedCharacters.count("Harry") > 10
+    assert list(set(tokenizedCharacters)).__len__() == 24
+
+
+def test_text_of_harry_potter_chamber_of_secret():
+    tokenizedCharacters = main.tokenize_harry_potter_book_chamber_of_secrets()
+
+    assert tokenizedCharacters.__len__() > 0
+    assert tokenizedCharacters.count("Harry") > 10
+    assert list(set(tokenizedCharacters)).__len__() == 28
