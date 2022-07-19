330d10979966e247517330b479894e30103e409f
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2022-07-18 19:24:26 -0500

    -Found a way to get proper data lined up in bar chart -Removed punctuation from book test to make regex of character names more reliable

--- main.py
+++ main.py
@@ -4,28 +4,40 @@ import matplotlib.pyplot as plt
 import requests
 
 
-def entropy(labels):
-    freqdist = nltk.FreqDist(labels)  # Will count symbols in a set
-    probs = [freqdist.freq(l) for l in freqdist]  # Will calculate the probability of said count
-    plt.bar(freqdist.keys(), probs)
+def entropy(names):
+    freqDistOfNames = nltk.FreqDist(names)  # Will count symbols in a set
+    names = [name for name in freqDistOfNames]
+    probs = [freqDistOfNames.freq(name) for name in freqDistOfNames]  # Will calculate the probability of said count
+
+    plt.bar(names, probs)
+    plt.xticks(rotation=60)
     plt.show()
+
     return -sum(p * math.log(p, 2) for p in probs)  # Calculates entropy for set
 
 
 def tokenize_harry_potter_book_philosopher_stone():
     url = "https://raw.githubusercontent.com/formcept/whiteboard/master/nbviewer/notebooks/data/harrypotter/Book%201" \
           "%20-%20The%20Philosopher's%20Stone.txt"
-    response = requests.get(url)
-    tokenizer = nltk.tokenize.RegexpTokenizer(get_regex_for_all_characters())
-    return tokenizer.tokenize(response.text)
+    bookText = requests.get(url).text
+
+    removePunctuationTokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+    bookTextPunctuationRemoved = removePunctuationTokenizer.tokenize(bookText)
+
+    getCharacterNamesTokenizer = nltk.tokenize.RegexpTokenizer(get_regex_for_all_characters())
+    return getCharacterNamesTokenizer.tokenize(' '.join(bookTextPunctuationRemoved))
 
 
 def tokenize_harry_potter_book_chamber_of_secrets():
     url = "https://raw.githubusercontent.com/formcept/whiteboard/master/nbviewer/notebooks/data/harrypotter/Book%202" \
           "%20-%20The%20Chamber%20of%20Secrets.txt"
-    response = requests.get(url)
-    tokenizer = nltk.tokenize.RegexpTokenizer(get_regex_for_all_characters())
-    return tokenizer.tokenize(response.text)
+    bookText = requests.get(url).text
+
+    removePunctuationTokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+    bookTextPunctuationRemoved = removePunctuationTokenizer.tokenize(bookText)
+
+    getCharacterNamesTokenizer = nltk.tokenize.RegexpTokenizer(get_regex_for_all_characters())
+    return getCharacterNamesTokenizer.tokenize(' '.join(bookTextPunctuationRemoved))
 
 
 def get_harry_potter_characters():
