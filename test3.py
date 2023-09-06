a7238f5cb86a77907a4627a81b4d7a760ac90459
Ryan Llamas <49294244+LlamasOnTheRun@users.noreply.github.com>
2023-09-05 23:41:55 -0500

    Hugging Face Practice

--- test3.py
+++ test3.py
@@ -15,3 +15,16 @@ input_ids = tokenizer.convert_tokens_to_ids(tokens)
 print(input_ids)
 final_inputs = tokenizer.prepare_for_model(input_ids)  # Will add special tokens
 print(final_inputs["input_ids"])
+
+print("##################### LEARNING TOKENIZATION WITH BATCHING INPUTS #####################")
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+sentences = [
+    "I've been waiting for a HuggingFace course my whole life.",
+    "I love this!",
+]
+tokens = [tokenizer.tokenize(sentence) for sentence in sentences]
+ids = [tokenizer.convert_tokens_to_ids(token) for token in tokens]
+print(ids[0])
+print(ids[1])
